719348810cc11460157b2ce2047735e5f2d40e61
amrrhassan <amrhassan98@hotmail.com>
2023-07-24 20:38:23 +0300

    edit server for frontend

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -60,7 +60,7 @@ const Customizer = () => {
     try {
       // call backend
       setGeneratingImg(true);
-      const response = await fetch("http://localhost:8080/api/v1/dalle", {
+      const response = await fetch("https://izzy3d.onrender.com/api/v1/dalle", {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
