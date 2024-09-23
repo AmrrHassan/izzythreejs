c71468fb81759f57b3b1c2e8c1e0cc090d530ed9
amrrhassan <amrhassan98@hotmail.com>
2024-09-23 22:15:52 +0300

    remove openai

--- server/routes/dalle.routes.js
+++ server/routes/dalle.routes.js
@@ -7,7 +7,8 @@ dotenv.config();
 const router = express.Router();
 
 const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+  // apiKey: process.env.OPENAI_API_KEY,
+  apiKey: 'preventUseOfapiKeyToThisProject',
 });
 const openai = new OpenAIApi(configuration);
 
